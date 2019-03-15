88117781b0aee36f590a6f9c90c1c4181bb58eb2
Max Cong <savagecm@users.noreply.github.com>
2019-03-15 15:23:20 +0800

    Create v4.cpp

---
+++ include/singleton/v4.cpp
@@ -0,0 +1,15 @@
+std::atomic<Singleton*> Singleton::m_instance;
+std::mutex Singleton::m_mutex;
+
+Singleton* Singleton::getInstance() {
+    Singleton* tmp = m_instance.load(std::memory_order_acquire);
+    if (tmp == nullptr) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        tmp = m_instance.load(std::memory_order_relaxed);
+        if (tmp == nullptr) {
+            tmp = new Singleton;
+            m_instance.store(tmp, std::memory_order_release);
+        }
+    }
+    return tmp;
+}
